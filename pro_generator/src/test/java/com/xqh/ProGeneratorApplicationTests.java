4d9e80ce2a6a2fbb93d429d5f5c77e4f18d2a026
tom_glb <goodMorning_pro@qq.com>
2021-09-16 23:40:15 +0800

    提交代码生成项目

---
+++ pro_generator/src/test/java/com/xqh/ProGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xqh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
